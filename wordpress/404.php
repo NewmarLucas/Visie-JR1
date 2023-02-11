283286401b223a46dbe1263c3d88b59a673c65b3
NewmarLucas <newmarlucas@gmail.com>
2023-02-11 11:02:01 -0400

    feat: adding worpress folder to repo

---
+++ wordpress/404.php
@@ -0,0 +1,13 @@
+<?php wp_enqueue_style('style', get_stylesheet_uri()); ?>
+<?php get_header(); ?>
+<div class="hero is-fullheight-with-navbar is-link">
+  <div class="hero-body">
+    <div class="container">
+      <span class="is-size-1 has-text-weight-semibold">¯\_(ツ)_/¯</span>
+      <p class="title">Página não encontrada</p>
+      <p class="subtitle">O conteúdo que você buscou não foi encontrado</p>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ wordpress/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Desenvolvido por
+      <a href="https://github.com/newmarlucas" target="_blank">Newmar Lucas</a>
+      &copy; <?php echo date("Y"); ?>
+    </p>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</div>
+</body>
+
+</html>
